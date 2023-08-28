91367bdc7580a524a4e2a8c2b0b3d603792c28c3
ITedInnovator <anthony.church@slickbluedragon.co.uk>
2023-08-28 21:09:53 +0100

    Remove dashicons from frontend and stop Google fonts loading.

--- functions.php
+++ functions.php
@@ -34,3 +34,14 @@ function hello_elementor_child_scripts_styles() {
 
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
+
+// remove dashicons
+function wpdocs_dequeue_dashicon() {
+	if (current_user_can( 'update_core' )) {
+	    return;
+	}
+	wp_deregister_style('dashicons');
+}
+add_action( 'wp_enqueue_scripts', 'wpdocs_dequeue_dashicon' );
+
+add_filter('elementor/frontend/print_google_fonts', '__return_false');
